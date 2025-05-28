284e0dd05419e18ff2fa873cffa10f011186c941
combinativa <ag.combinativa@gmail.com>
2025-05-28 16:37:25 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Bem-vindo ao App-MCA</h1>
+      <h1>Bem-vindo ao App-MCA!</h1>
       <p>Seu app está online e rodando com Next.js na Vercel! 🚀</p>
     </div>
   );
